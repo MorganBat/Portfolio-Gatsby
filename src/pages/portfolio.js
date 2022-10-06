f1c330be484320d2dfed328b72fb18043d1e6129
Morgan Batterham <morgan@morganbatterham.me>
2022-10-07 10:28:22 +1100

    work on Portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -9,6 +9,11 @@ const Portfolio = () => {
             <div>
                 <h1>Portfolio Page</h1>
             </div>
+            <div>
+                <h2>Fitr - Workout tracker</h2>
+                Fitr was a project built for my final assessment while undertaking a coding bootcamp at Coder Academy. Build on a Ruby on Rails backend with a PostgreSQL database, the frontend was made in React.JS.
+                <a href="https://fitr-frontend.netlify.app/" target='_blank'>Live Link</a>
+            </div>
         </Layout>
     )
 }
